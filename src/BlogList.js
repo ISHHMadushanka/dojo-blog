62d3d46a8c7f39b46ed8e8b5f6f08543fd6b36f4
Ishara Madushanka <isharamadushanka240@gmail.com>
2022-07-10 23:47:29 +0530

    initial commit

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,11 +6,11 @@ const BlogList = ({ blogs, title }) => {
          {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
+                <p>Written by { blog.author }</p>                
             </div>
          ))}
        </div> 
      );  
-}
+}   
  
 export default BlogList;
